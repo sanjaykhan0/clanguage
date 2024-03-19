fae8e16775de566d1a7ae0106911141bb9cdd4a8
sanjaykhan0 <sanjaykhan2228@gmail.com>
2024-03-19 15:30:57 +0530

    lopping-3

---
+++ Looping-3/2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main (){
+    int n,sum=1;
+    scanf("%d",&n);
+    for(int i=1;i<n; i++){
+        sum*=i;
+        printf("all sum: %d\n",sum);
+    }
+        printf("total sum %d",sum);
+    
+}
